1a5671cad5c561bd8adae023f554d93b15c29fba
shawerob <132010673+shawerob@users.noreply.github.com>
2023-05-04 22:32:53 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(0)
+    radio.sendNumber(1)
 })
 input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(2)
+    radio.sendNumber(3)
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(1)
+    radio.sendNumber(2)
 })
 radio.setGroup(200)
